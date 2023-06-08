c4f5b21416652757bee2ed669e231fbe84dda9e7
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-06-08 15:50:18 -0300

    feat: use ENV to admin password

--- src/lib/create-admin.ts
+++ src/lib/create-admin.ts
@@ -1,9 +1,10 @@
+import { env } from '../config/env'
 import { prisma } from './prisma'
 import { hash } from 'bcrypt'
 
 // eslint-disable-next-line
 (async () => {
-  const password = await hash('test-password', 6)
+  const password = await hash(env.ADMIN_PASSWORD, 6)
 
   const adminCount = await prisma.syndicate.count()
   const username = `admin-${adminCount + 1}`
